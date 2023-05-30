21b9e79686b5638b4b26aaa8239052deaed1d6d2
sakotaka <134421966+sakotaka@users.noreply.github.com>
2023-05-30 15:31:11 +0900

    first commit.
    kmocy HPです。

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $('.event_img-container').slick({
+        dots: true,
+        arrows:false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 500,
+        fade: false,
+        cssEase: 'linear',
+        slidesToShow: 3,//スライドを画面に3枚見せる
+		    slidesToScroll: 1,
+        centerMode: true, // 両サイドに前後のスライド表示
+        centerPadding: '0px'
+    });
+});
+window.addEventListener("scroll", function () {
+  const elm = document.querySelector("#header");
+  const scroll = window.pageYOffset;
+  if (scroll > 200 || scroll == 0) {
+    elm.style.opacity = "1";
+    // console.log(scroll);
+  } else {
+    elm.style.opacity = "0";
+    // console.log(scroll);
+  }
+});
+window.addEventListener("scroll", function () {
+  const elm = document.querySelector("#LINE");
+  const scroll = window.pageYOffset;
+  if (scroll > 700) {
+    elm.style.opacity = "1";
+    // console.log(scroll);
+  } else {
+    elm.style.opacity = "0";
+    // console.log(scroll);
+  }
+});
